9711ae4060bfb138164403c11fe21c6c6c1f51ac
MariaSantana10 <133781356+MariaSantana10@users.noreply.github.com>
2023-10-23 08:30:00 -0300

    Update encapsulamneto-atividade-slide.py

--- encapsulamneto-atividade-slide.py
+++ encapsulamneto-atividade-slide.py
@@ -3,6 +3,35 @@ class maquiagem:
         self.batom=batom
         self.sombra=sombra
     
+    # def get_batom(self):
+    #     return self.batom
+    # def get_sombra(self):
+    #     return self.sombra
+
+    # def set_batom(self,batom):
+    #     self.batom=batom
+    # def set_sombra(self,sombra):
+    #     self.sombra=sombra
+
+    @property
+    def batom (self):
+        return self.__batom
+    @batom.setter
+    def batom (self,batom):
+        self.__batom=batom
+    
+    @property
+    def sombra(self):
+        return self.__sombra
+    @sombra.setter
+    def sombra(self,sombra):
+        self.__sombra=sombra
 
 
-produção = maquiagem ('vermelho','glitter')
+cor_batom= str(input('Qual a cor do batom?'))
+cor_sombra= str (input('Qual a cor da sombra?'))
+produção = maquiagem (cor_batom,cor_sombra)
+print(produção.batom)
+mudar_batom= str(input('Mude a cor do batom:'))
+produção.batom=mudar_batom
+print(produção.batom)
