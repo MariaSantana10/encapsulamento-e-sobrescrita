af9c9c269bb1a9542a09ef297647a9d24c0165c7
MariaSantana10 <c.santana@escolar.ifrn.edu.br>
2023-10-23 21:54:43 -0300

    Update encapsulamneto-atividade-slide.py
    Encapsulamento e sobrescrita completo

--- encapsulamneto-atividade-slide.py
+++ encapsulamneto-atividade-slide.py
@@ -37,44 +37,7 @@
 # print(produção.batom)
 
 
-#sobrescirta
-
-
-class A (object):
-    atributo_a= ' valor a'
-
-    def __init__(self, atributo):
-        self.atributo=atributo
-
-    def metodo(self):
-        print('A')
-    
-
-class B (A):
-    atributo_b= ' valor b'
-
-    def __init__(self, atributo,outra_coisa):
-        super().__init__(atributo)
-        self.outra_coisa=outra_coisa
-
-    def metodo(self):
-        print('B')
-    
-
-class C (B):
-    atributo_c= ' valor c'
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args,**kwargs)
-        self.atributo=atributo
-
-    def metodo(self):
-        A.metodo(self)
-        B.metodo(self)
-        print('C')
-    
-
-
+#sobrescrita
 
 class Integrante_IFRN:
     def __init__(self,nome,idade):
